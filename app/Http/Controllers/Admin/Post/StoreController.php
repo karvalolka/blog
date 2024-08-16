3c4580546e819c8720fad1bcdc80552f9103ca80
ChiLo <pyha1994@yandex.ru>
2024-08-16 19:19:48 +0300

    + апдейт у постовж + форма редактирования + отображение изображений в редактировании

--- app/Http/Controllers/Admin/Post/StoreController.php
+++ app/Http/Controllers/Admin/Post/StoreController.php
@@ -20,8 +20,8 @@ class StoreController extends Controller
             unset($data['tag_ids']);
 
 
-            $data['preview_image'] = Storage::put('/image', $data['preview_image']);
-            $data['main_image'] = Storage::put('/image', $data['main_image']);
+            $data['preview_image'] = Storage::disk('public')->put('/image', $data['preview_image']);
+            $data['main_image'] = Storage::disk('public')->put('/image', $data['main_image']);
             $post = Post::firstOrCreate($data);
             $post->tags()->attach($tagIds);
         } catch (\Exception $exception) {
